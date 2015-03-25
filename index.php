338a9a9c8427ac89d68bac3d58042cca2dc7daba
Nicolas Husser <nicolas.husser@gmail.com>
2015-03-24 21:42:52 -0400

    ADD POST. OMG.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 	            <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
 	            <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
 	            <p id="profile-name" class="profile-name-card"></p>
-	            <form class="form-signin" accept-charset="UTF-8" action="includes/login.php">
+	            <form method="POST" class="form-signin" accept-charset="UTF-8" action="includes/login.php">
 	                <span id="reauth-email" class="reauth-email"></span>
 	                <input type="text"     name="email" id="email" class="form-control" placeholder="Email address" required autofocus>
 	                <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
