0bac34724c3f41dd48abf57cefa14ee33447ab48
Nicolas Husser <nicolas.husser@free.fr>
2015-04-02 10:26:23 -0400

    Add second DB migration file

---
+++ DBMigration2.sql
@@ -0,0 +1,15 @@
+
+DELETE FROM users;
+
+ALTER TABLE users DROP EmployeeID;
+ALTER TABLE users ADD EmployeeID INT NOT NULL ;
+ALTER TABLE users ADD FOREIGN KEY (EmployeeID) REFERENCES Employee(ID);
+
+INSERT INTO users (email,password,username,privelege,EmployeeID)
+        VALUES ("vince.abru@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","VinceAbruzzese","admin",5001),
+                ("jordan.rooks@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","JordanRooks","regular",5002),
+                ("andrew.costa@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","AndrewCosta","regular",5004),
+                ("nicolas.husser@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","NicolasHusser","admin",5003);
+
+
+
