0286d716246cf5aad267f51bb1ef7a85e4f74ce8
Nicolas Husser <nicolas.husser@free.fr>
2015-04-02 10:11:52 -0400

    Working DB migration version

--- DBMigration1.sql
+++ DBMigration1.sql
@@ -1,15 +1,12 @@
 
-ALTER TABLE users DROP COLUMN id;
-ALTER TABLE users ADD EmployeeID INT(11) NOT NULL BEFORE email
-			ADD CONSTRAINT FOREIGN KEY(EmployeeID) REFERENCES Employee(ID);
+ALTER TABLE users ADD EmployeeID INT(11) NOT NULL REFERENCES Employee(ID);
 
-
-DELETE * FROM users;
-INSERT INTO users (email,password,username,privelege) 
-	VALUES (5001,"vince.abru@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","VinceAbruzzese","admin"),
-		(5002,"jordan.rooks@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","JordanRooks","regular"),
-		(5004,"andrew.costa@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","AndrewCosta","regular"),
-		(5003,"nicolas.husser@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","NicolasHusser","admin");
+DELETE FROM users;
+INSERT INTO users (email,password,username,privelege,EmployeeID) 
+	VALUES ("vince.abru@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","VinceAbruzzese","admin",5001),
+		("jordan.rooks@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","JordanRooks","regular",5002),
+		("andrew.costa@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","AndrewCosta","regular",5004),
+		("nicolas.husser@gmail.com","8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918","NicolasHusser","admin",5003);
  
 
 
