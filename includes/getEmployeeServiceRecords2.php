fd88c5c4de22a45703c60d517942c281d98e318c
Nicolas Husser <nicolas.husser@gmail.com>
2015-04-07 21:57:56 -0400

    Debug

--- includes/getEmployeeServiceRecords2.php
+++ includes/getEmployeeServiceRecords2.php
@@ -23,31 +23,98 @@ try {
 $ID = $_GET["id"];
 $TYPE = $_GET["type"];
 
-if ($ID == "") {
-    if ($TYPE == "Sale")
-        $sql = $db->prepare("SELECT ProductID, Name, Price, Total FROM (SELECT ProductID, COUNT(ProductID) as Total From Sale GROUP BY ProductID ORDER BY Total DESC ) as T1, (SELECT ID, Name, Price FROM PC UNION SELECT ID, Name, Price FROM Laptop UNION SELECT ID, Name, Price FROM Software UNION SELECT ID, Name, Price FROM Part) as T2 WHERE T1.ProductID = T2.ID;");
-    else if ($TYPE == "Repair")
-        $sql = $db->prepare("SELECT * FROM Repair ");
-    else if ($TYPE == "OnlineSale")
-        $sql = $db->prepare("SELECT * FROM OnlineSale");
-    else if ($TYPE == "Install")
-        $sql = $db->prepare("SELECT * FROM Install");
-    else if ($TYPE == "Upgrade")
-        $sql = $db->prepare("SELECT * FROM Upgrade");
-} else {
-    if ($TYPE == "Sale")
-        $sql = $db->prepare("SELECT ProductID, Name, Price, Total FROM (SELECT ProductID, COUNT(ProductID) as Total From Sale GROUP BY ProductID ORDER BY Total DESC ) as T1, (SELECT ID, Name, Price FROM PC UNION SELECT ID, Name, Price FROM Laptop UNION SELECT ID, Name, Price FROM Software UNION SELECT ID, Name, Price FROM Part) as T2 WHERE T1.ProductID = T2.ID;");
-    else if ($TYPE == "Repair")
-        $sql = $db->prepare("SELECT * FROM Repair WHERE EmployeeID = ?");
-    else if ($TYPE == "OnlineSale")
-        $sql = $db->prepare("SELECT * FROM OnlineSale WHERE EmployeeID = ?");
-    else if ($TYPE == "Install")
-        $sql = $db->prepare("SELECT * FROM Install WHERE EmployeeID = ?");
-    else if ($TYPE == "Upgrade")
-        $sql = $db->prepare("SELECT * FROM Upgrade WHERE EmployeeID = ?");
 
-    $sql->bindValue(1, $ID);
-}
+if ($TYPE == "Sale")
+    $sql = $db->prepare("SELECT ProductID, Name, Price, Total
+                            FROM (SELECT ProductID, COUNT(ProductID) as Total
+                            From Sale
+                            GROUP BY ProductID
+                            ORDER BY Total DESC
+                            ) as T1, (SELECT ID, Name, Price
+                            FROM PC
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Laptop
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Software
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Part) as T2
+                            WHERE T1.ProductID = T2.ID;");
+else if ($TYPE == "Repair")
+    $sql = $db->prepare("SELECT ComputerID, Name, Price, Total
+                            FROM (SELECT ComputerID, COUNT(ComputerID) as Total
+                            From Repair
+                            GROUP BY ComputerID
+                            ORDER BY Total DESC
+                            ) as T1, (SELECT ID, Name, Price
+                            FROM PC
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Laptop
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Software
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Part) as T2
+                            WHERE T1.ComputerID = T2.ID;");
+else if ($TYPE == "OnlineSale")
+    $sql = $db->prepare("SELECT ProductID, Name, Price, Total
+                            FROM (SELECT ProductID, COUNT(ProductID) as Total
+                            From OnlineSale
+                            GROUP BY ProductID
+                            ORDER BY Total DESC
+                            ) as T1, (SELECT ID, Name, Price
+                            FROM PC
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Laptop
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Software
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Part) as T2
+                            WHERE T1.ProductID = T2.ID;");
+else if ($TYPE == "Install")
+    $sql = $db->prepare("SELECT SoftwareID, Name, Price, Total
+                            FROM (SELECT SoftwareID, COUNT(SoftwareID) as Total
+                            From Install
+                            GROUP BY SoftwareID
+                            ORDER BY Total DESC
+                            ) as T1, (SELECT ID, Name, Price
+                            FROM PC
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Laptop
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Software
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Part) as T2
+                            WHERE T1.SoftwareID = T2.ID;");
+else if ($TYPE == "Upgrade")
+    $sql = $db->prepare("SELECT ComputerID, Name, Price, Total
+                            FROM (SELECT ComputerID, COUNT(ComputerID) as Total
+                            From Upgrade
+                            GROUP BY ComputerID
+                            ORDER BY Total DESC
+                            ) as T1, (SELECT ID, Name, Price
+                            FROM PC
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Laptop
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Software
+                            UNION
+                            SELECT ID, Name, Price
+                            FROM Part) as T2
+                            WHERE T1.ComputerID = T2.ID;");
+
 
 $sql->execute();
 $rows = $sql->fetchALL();
